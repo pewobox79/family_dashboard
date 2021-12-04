c06eefc2c024b1616fe325f6cafac996bc38e386
Peter Wolf <pewobox@Peters-Air.fritz.box>
2021-12-04 17:48:33 +0100

    included prettier

---
+++ components/MainBoard.tsx
@@ -0,0 +1,18 @@
+import {styled} from '@mui/material/styles';
+
+export default function MainBoard() {
+
+    const Background = styled('div')`
+      height: 100vh;
+      width: 100%;
+      background-color: #23a8ba;
+    `
+
+
+    return (
+
+        <Background>
+            <h1>MainBoard</h1>
+        </Background>
+    )
+}
